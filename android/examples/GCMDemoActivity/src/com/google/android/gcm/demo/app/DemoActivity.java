85e4e924204d63c5a0d4c6eb98633fb1099c67d6
Devendra <devendra@udgama.com>
2013-06-20 22:04:59 +0530

    gcm demo app

---
+++ android/examples/GCMDemoActivity/src/com/google/android/gcm/demo/app/DemoActivity.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.android.gcm.demo.app;
+
+import static com.google.android.gcm.demo.app.CommonUtilities.DISPLAY_MESSAGE_ACTION;
+import static com.google.android.gcm.demo.app.CommonUtilities.EXTRA_MESSAGE;
+import static com.google.android.gcm.demo.app.CommonUtilities.SENDER_ID;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.gcm.GCMRegistrar;
+import com.pubnub.api.Callback;
+import com.pubnub.api.Pubnub;
+
+/**
+ * Main UI for the demo app.
+ */
+public class DemoActivity extends Activity {
+	
+	static Activity da;
+
+	TextView mDisplay;
+	AsyncTask<Void, Void, Void> mRegisterTask;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		da = this;
+		checkNotNull(SENDER_ID, "SENDER_ID");
+		GCMRegistrar.checkDevice(this);
+		setContentView(R.layout.main);
+		mDisplay = (TextView) findViewById(R.id.display);
+		registerReceiver(mHandleMessageReceiver,
+				new IntentFilter(DISPLAY_MESSAGE_ACTION));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.options_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Pubnub pubnub;
+		JSONObject jso;
+		String regId;
+		switch(item.getItemId()) {
+		case R.id.options_register:
+			GCMRegistrar.register(this, SENDER_ID);
+			return true;
+		case R.id.options_unregister:
+			GCMRegistrar.unregister(this);
+			return true;
+
+		case R.id.options_clear:
+			mDisplay.setText(null);
+			return true;
+		case R.id.options_exit:
+			finish();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		if (mRegisterTask != null) {
+			mRegisterTask.cancel(true);
+		}
+		unregisterReceiver(mHandleMessageReceiver);
+		GCMRegistrar.onDestroy(this);
+		super.onDestroy();
+	}
+
+	private void checkNotNull(Object reference, String name) {
+		if (reference == null) {
+			throw new NullPointerException(
+					getString(R.string.error_config, name));
+		}
+	}
+
+	private final BroadcastReceiver mHandleMessageReceiver =
+			new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			String newMessage = intent.getExtras().getString(EXTRA_MESSAGE);
+			mDisplay.append(newMessage + "\n");
+		}
+	};
+
+}
